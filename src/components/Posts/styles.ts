d9a752253b213c9f67e1630064c103965fa706a3
ThiagoLG <thiago.lg53@gmail.com>
2023-09-02 15:31:47 -0300

    feat: created Post component (only structure and style)

---
+++ src/components/Posts/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const PostContainer = styled.article`
+  height: 16.25rem;
+  flex-shrink: 0;
+  border-radius: 0.625rem;
+  padding: 2rem;
+  background: ${({ theme }) => theme['base-post']};
+  display: grid;
+  grid-template-columns: 1fr 5rem;
+  grid-template-rows: 3.75rem auto;
+  gap: 1.25rem;
+
+  h1 {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    color: ${({ theme }) => theme['base-title']};
+    font-size: 1.25rem;
+    font-weight: 700;
+    line-height: 160%;
+    grid-column: 1/2;
+  }
+
+  span {
+    color: ${({ theme }) => theme['base-span']};
+    margin-top: 0.31rem;
+    font-size: 0.875rem;
+    line-height: 160%;
+    grid-column: 2 / 3;
+    text-align: right;
+  }
+
+  p {
+    color: ${({ theme }) => theme['base-text']};
+    display: -webkit-box;
+    -webkit-line-clamp: 5;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+
+    grid-column: 1/3;
+
+    font-size: 1rem;
+    line-height: 150%;
+  }
+`
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,12 +1,21 @@
+import { Post } from '../../components/Posts'
 import { Profile } from '../../components/Profile'
 import { SearchForm } from '../../components/SearchForm'
-import { BlogContainer } from './styles'
+import { BlogContainer, PostsContainer } from './styles'
 
 export function Blog() {
   return (
     <BlogContainer>
       <Profile />
       <SearchForm />
+      <PostsContainer>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </PostsContainer>
     </BlogContainer>
   )
 }
